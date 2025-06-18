bee8a52974b7cd48412262af16153bfe0aa236f3
jnovoadeuna <jnovoa@deuna.com>
2025-06-18 16:39:41 -0500

    fix: add playwright report html

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,8 +24,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['list'],                 // puedes mantenerlo si quieres ver resultados en consola
-    ['allure-playwright']
+    ['html'],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
